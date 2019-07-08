33a58b4b0b3a3153f61ccdedb7251462a663036e
Full Name <nickfuentes24@gmail.com>
2019-07-07 21:47:30 -0500

    Showing pool tables avaible to book.

--- pool-table.py
+++ pool-table.py
@@ -1,21 +1,31 @@
-user_input = ""
-
-
 def print_pool_table_title():
-    print("---------------------")
-    print("UH Pool Table Manager")
-    print("---------------------")
+    print("---------------------------")
+    print("-- UH Pool Table Manager --")
+    print("---------------------------")
 
 
 def pool_table_menu():
     print("Press [1] to View Pool Tables")
     print("Press [2] to Occupy Pool Table")
     print("Press [3] to Un-Occupy Pool Table")
+    print("Press [q] to Quit")
 
 
 print_pool_table_title()
+user_input = ""
 
-
+tables = []
 while user_input != "q":
-    user_input == input("What would you like to do? ")
+
     pool_table_menu()
+    user_input = input("What would you like to do? ")
+    if user_input == "1":
+        print("---------------------")
+        print("---- Pool Tables ----")
+        print("---------------------")
+        for table in range(0, 13):
+            print(f"Table {table+1}")
+        print("---------------------")
+    elif user_input == "2":
+        occupy_input = input(
+            "Type in number of pool table you would like to occupy. ")
